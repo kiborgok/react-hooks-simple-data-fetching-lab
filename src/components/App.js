aadedc1f75db540236c915a5c5f492d3f78ab19e
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-04 21:18:29 +0300

    use useEffect to fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,17 @@
-// create your App component here
+import { useState, useEffect } from "react";
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState("");
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setImage(data.message);
+      });
+  }, []);
+
+  return loading ? <p>Loading...</p> : <img src={image} alt="A Random Dog" width="400px" />;
+}
+export default App;
